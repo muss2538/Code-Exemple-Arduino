769aff0870b903fb586d1d862cb1c09846106893
muss2538 <marut_budd@hotmail.com>
2016-08-08 15:44:30 +0700

    Add files via upload

---
+++ testI2CRTCLCD.ino
@@ -0,0 +1,46 @@
+#include <Wire.h>
+#include "RTClib.h"
+#include <LiquidCrystal_I2C.h>
+
+
+RTC_DS1307 rtc;    // Create a RealTimeClock object
+
+LiquidCrystal_I2C lcd(0x3F, 20, 4);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("YourDuino.com DS1307 Real Time Clock - Set / Run Utility");
+  Wire.begin();
+  rtc.begin();
+  lcd.begin();
+
+}
+
+void loop(){
+  DateTime now = rtc.now();  // Read data from the RTC Chip
+
+//  Serial.print(now.year(), DEC); //
+//  Serial.print('/');
+//  Serial.print(now.month(), DEC);
+//  Serial.print('/');
+//  Serial.print(now.day(), DEC);
+//  Serial.print(' ');
+//  Serial.print(now.hour(), DEC);
+//  Serial.print(':');
+//  Serial.print(now.minute(), DEC);
+//  Serial.print(':');
+//  Serial.print(now.second(), DEC);
+//  Serial.println();
+
+  String timeread = "Time = "+String(now.hour())+":"+String(now.minute())+":"+String(now.second());
+  String dateread = "Date = "+String(now.day())+"/"+String(now.month())+"/"+String(now.year());
+  lcd.setCursor(0,1);
+  lcd.print(timeread);
+  lcd.setCursor(0,2);
+  lcd.print(dateread);
+
+
+  delay(1000);
+  lcd.clear();
+}
+
---
+++ testkeypad4x4lcdi2c.ino
@@ -0,0 +1,36 @@
+//Original Creator: Andrew Mascolo
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <OnewireKeypad.h>
+
+char KEYS[]= {
+  '1','2','3','A',
+  '4','5','6','B',
+  '7','8','9','C',
+  '*','0','#','D'
+};
+LiquidCrystal_I2C Lcd(0x3F,20,4); 
+OnewireKeypad <LiquidCrystal_I2C, 16> KP( Lcd, KEYS, 4, 4, A1, 4700, 1000 );
+//OnewireKeypad <Print, 12 > KP2(Serial, KEYS, 4, 3, A0, 4700, 1000 );
+
+void setup (){
+  Serial.begin(9600);
+  Lcd.begin();                      
+
+}
+void loop() 
+{
+  if( KP.Getkey() )
+  {
+    Lcd.clear();
+    Lcd.home();
+    Lcd.print( F( "KP: " ) );
+    Lcd.setCursor(4,0);
+    Lcd.print(KP.Getkey());Serial.println(KP.Getkey());
+    delay(1000);
+    
+  }
+
+}
+
