1e371f2055760dbec940b1329b678f462c2b1976
muss2538 <marut_budd@hotmail.com>
2016-08-17 19:44:53 +0700

    Update Realv.1.ino

--- Realv.1.ino
+++ Realv.1.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>
 #include "RTClib.h"
-#include "HX711.h"
+//#include "HX711.h"
 #include <LiquidCrystal_I2C.h>
 #include <OnewireKeypad.h>
 
@@ -13,27 +13,33 @@ int yyear = 0;
 int ho = 0;
 int mi = 0;
 int se = 0;
+
 char timearray[] = "000000";
+char manyarray[] = "000000";
+char volumearray[] = "000000";
 byte counttimearray = 0;
+byte countmanyarray = 0;
+byte countvolumearray = 0;
+
 byte statemenu = 0;
 byte slectmenu = 1;
 byte i=9;
 
-float calibration_factor =99757.00; 
-#define zero_factor 8573573
-#define DOUT  A3
-#define CLK   A2
-#define DEC_POINT  2
+//float calibration_factor =99757.00; 
+//#define zero_factor 8573573
+//#define DOUT  A3
+//#define CLK   A2
+//#define DEC_POINT  2
 float offset=0;                 float get_units_kg();
-HX711 scale(DOUT, CLK);
+//HX711 scale(DOUT, CLK);
 
 LiquidCrystal_I2C lcd(0x3F, 20, 4);
 RTC_DS1307 rtc;
 OnewireKeypad <LiquidCrystal_I2C, 16> KP( lcd, KEYS, 4, 4, A1, 4700, 1000 );
 
-float get_units_kg() {
-  return(scale.get_units()*0.453592);
-}
+//float get_units_kg() {
+//  return(scale.get_units()*0.453592);
+//}
 void openmenu() {
   lcd.clear(); delay(1000);
 }
@@ -55,13 +61,14 @@ void distime() {
   ho =(timearray[0]*10)+timearray[1];
   mi =(timearray[2]*10)+timearray[3];
   se =(timearray[4]*10)+timearray[5];
-  lcd.print("Time = ");
-  lcd.print(ho);
-  lcd.print(":");
-  lcd.print(mi);
-  lcd.print(":");
-  lcd.print(se);
+  lcd.print("Time = ");lcd.print(ho);lcd.print(":");lcd.print(mi);lcd.print(":");lcd.print(se);
 }
+void dismany() {
+  
+}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+void disvolume() {
+  
+}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 void menu() {
   if (slectmenu == 1) {
     lcd.setCursor(0, 0);    lcd.print(">> Set Date");
@@ -87,7 +94,7 @@ void menu() {
     lcd.setCursor(0, 2);    lcd.print("   Set Many Shrimp");
     lcd.setCursor(0, 3);    lcd.print(">> Set Volume");
     }
-}
+}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 void MenuSetDate() {
   openmenu();
   DateTime now = rtc.now();
@@ -120,30 +127,26 @@ void MenuSetTime() {
   }
 }
 void MenuSetManyShrimp() {
-  lcd.clear();
-  delay(1000);
+  openmenu();
   lcd.setCursor(0, 0);
   lcd.print("***Many Shrimp***");
   if (KP.Getkey() != NO_KEY){
     lcd.setCursor(i, 2);    
     lcd.print(KP.Getkey());
-    timearray[counttimearray] = KP.Getkey();
-    counttimearray++;i++;
+    timearray[countmanyarray] = KP.Getkey();
+    countmanyarray++;i++;
     delay(150);
     if (counttimearray == 4){
-      lcd.clear();
-      delay(1000);
+      openmenu();
       lcd.setCursor(6, 1); 
-      lcd.print("Time Saving");
-      delay(2000);
-      lcd.clear();
+      lcd.print("Saving ...");
       byte i=9; counttimearray=0;
+      closemenu();
     }
   }
-}
+}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 void MenuSetVolume() {
-  lcd.clear();
-  delay(1000);
+  openmenu();
   lcd.setCursor(0, 0);
   lcd.print("***SetVolume***");
   if (KP.Getkey() != NO_KEY){
@@ -153,44 +156,45 @@ void MenuSetVolume() {
     counttimearray++;i++;
     delay(150);
     if (counttimearray == 3){
-      lcd.clear();
-      delay(1000);
+      openmenu();
       lcd.setCursor(6, 1); 
-      lcd.print("Time Saving");
-      delay(2000);
-      lcd.clear();
+      lcd.print("Saving ...");
       byte i=9; counttimearray=0;
+      closemenu();
     }
   }
-}
+}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 /*****************************************************************************************************************************/
 void setup() {
   Serial.begin(9600);
   lcd.begin();
   Wire.begin();
   rtc.begin();
-  lcd.clear();
-  scale.set_scale(calibration_factor); 
-  scale.set_offset(zero_factor);
+  lcd.print("Deivce Power ON");
+  Serial.println("Deivce Power ON");
+  closemenu();
+  //scale.set_scale(calibration_factor); 
+  //scale.set_offset(zero_factor);
 }
 void loop() {
-  
-  String data = String(get_units_kg()+offset, DEC_POINT);
+  Serial.println("Loop intro");
+  //String data = String(get_units_kg()+offset, DEC_POINT);
   
 start:
   statemenu = 0;
   into();
   char keymenu = KP.Getkey();
   
-  
   if (keymenu == '*') {//Loop Checking *
     statemenu = 1;
     lcd.clear();delay(1000);
     while (statemenu == 1) {
-      //lcd.print(datearray);
-      lcd.print(timearray);
-      lcd.print(ManyShrimp);
-      lcd.print(Volume);
+      Serial.println("Loop Checking");
+      openmenu();
+      lcd.setCursor(0,0);       disdate();
+      lcd.setCursor(0,1);       distime();
+      lcd.setCursor(0,2);       lcd.print(ManyShrimp);
+      lcd.setCursor(0,3);       lcd.print(Volume);
       
       char fnmenu = KP.Getkey();
       if (fnmenu == 'D') {
@@ -207,6 +211,7 @@ start:
     statemenu = 1;
     lcd.clear();delay(1000);
     while (statemenu == 1) {
+      Serial.println("Loop Setup");
       delay(250);
       char fnmenu = KP.Getkey();
       if ((fnmenu == 'A')&&(slectmenu!=1)){   //UP
