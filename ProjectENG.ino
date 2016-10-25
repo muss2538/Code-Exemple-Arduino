41e03f4fc2e683db98d0c5859ff7e0c6f37b10fb
muss2538 <marut_budd@hotmail.com>
2016-10-25 21:58:32 +0700

    Update ProjectENG.ino

--- ProjectENG.ino
+++ ProjectENG.ino
@@ -1,4 +1,28 @@
-void setup(){
+int reada2,reada3;
+
+char ReadDataChoice(){
+  reada2 = analogRead(A2);
+  if((reada2 >= 565)&&(reada2 <= 568))return 'A';
+  if((reada2 >= 565)&&(reada2 <= 568))return 'B';
+  if((reada2 >= 565)&&(reada2 <= 568))return 'C';
+  if((reada2 >= 565)&&(reada2 <= 568))return 'D';
 }
+char ReadDataControl(){
+  reada3 = analogRead(A3);
+  if((reada2 >= 565)&&(reada2 <= 568))return '';
+  if((reada2 >= 565)&&(reada2 <= 568))return 'B';
+  if((reada2 >= 565)&&(reada2 <= 568))return 'C';
+  if((reada2 >= 565)&&(reada2 <= 568))return 'D';
+}
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  for(int pind=8;pind<=15;pind++){
+    pinMode(pind,OUTPUT);}
+}
+
 void loop(){
+  
 }
